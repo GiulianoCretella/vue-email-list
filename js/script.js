5a9bfc8a2d0be4cb4517a95d64fe60d4641f0e74
GiulianoCretella <giulianocretella.1@gmail.com>
2022-05-02 17:40:15 +0200

    genero i 10 indirizzi email e stampo a video

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+    el:'#app',
+    data:{
+     emailArray:[],
+    },
+    methods: {
+
+    },
+    created(){
+        for(let i = 0 ; i < 10; i++ )
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((res) =>{
+          this.emailArray.push(res.data.response)
+        }).catch(function (error) {
+          console.log(error);
+        })
+        console.log(this.emailArray)
+    }
+})
